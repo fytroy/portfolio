d8e0b0facdfccfd01e0431793013cc56c9da1d9e
Rodney Roy Gitonga <84504060+fytroy@users.noreply.github.com>
2025-12-04 09:26:22 +0300

    Initialize cursor-animations-and-web-text

---
+++ app/(root)/page.tsx
@@ -0,0 +1,257 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+import { AnimatedSection } from "@/components/common/animated-section";
+import { AnimatedText } from "@/components/common/animated-text";
+import { ClientPageWrapper } from "@/components/common/client-page-wrapper";
+import { Icons } from "@/components/common/icons";
+import ContributionCard from "@/components/contributions/contribution-card";
+import ProjectCard from "@/components/experience/project-card";
+import SkillsCard from "@/components/skills/skills-card";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { featuredContributions } from "@/config/contributions";
+import { featuredExperiences } from "@/config/experience";
+import { pagesConfig } from "@/config/pages";
+import { featuredSkills } from "@/config/skills";
+import { cn } from "@/lib/utils";
+import namanImg from "@/public/naman-img.jpg";
+// import {
+//     Accordion,
+//     AccordionContent,
+//     AccordionItem,
+//     AccordionTrigger,
+// } from "@/components/ui/accordion";
+// import {
+//     DropdownMenu,
+//     DropdownMenuContent,
+//     DropdownMenuLabel,
+//     DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu";
+
+export const metadata: Metadata = {
+  title: pagesConfig.home.metadata.title,
+  description: pagesConfig.home.metadata.description,
+};
+
+export default function IndexPage() {
+  return (
+    <ClientPageWrapper>
+      <section className="space-y-6 pb-8 pt-6 mb-0 md:pb-12 md:py-20 lg:py-32 h-screen flex items-center">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center -mt-20">
+          {/* <Link
+                        href={"siteConfig.links.twitter"}
+                        className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+                        target="_blank"
+                    >
+                        Follow along on Twitter
+                    </Link> */}
+          {/* <div className="max-w-[16rem]"> */}
+          <Image
+            src={namanImg}
+            height={100}
+            width={100}
+            sizes="100vw"
+            className="bg-primary rounded-full mb-0 h-auto md:mb-2 w-[60%] max-w-[16rem] border-8 border-primary"
+            alt="naman-barkiya-img"
+          />
+          {/* </div> */}
+          <AnimatedText
+            as="h1"
+            delay={0.2}
+            className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl"
+          >
+            Naman Barkiya
+          </AnimatedText>
+          <AnimatedText
+            as="h3"
+            delay={0.4}
+            className="font-heading text-base sm:text-xl md:text-xl lg:text-2xl"
+          >
+            Full Stack Developer
+          </AnimatedText>
+          {/* <Accordion
+                        type="single"
+                        collapsible
+                        className="flex items-center w-full max-w-[35rem]"
+                    >
+                        <AccordionItem value="main-ques" className="flex-1">
+                            <AccordionTrigger>
+                                <p className="flex items-center">
+                                    Can you guess my favorite coding problem?
+                                </p>
+                            </AccordionTrigger>
+                            <AccordionContent>
+                                &ldquo;Check if a given string is palindrome or
+                                not&rdquo;
+                                <Icons.laughEmoji className="w-4 h-5 md:w-5 ml-2" />
+                            </AccordionContent>
+                        </AccordionItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger
+                                asChild
+                                className="cursor-pointer"
+                            >
+                                <Icons.questionMark className="w-5 h-5 ml-3 text-muted-foreground " />
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-56">
+                                <DropdownMenuLabel className="flex items-center text-muted-foreground">
+                                    <Icons.infoMark className="w-4 h-4 mr-2" />
+                                    Hint: Naman
+                                </DropdownMenuLabel>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </Accordion> */}
+
+          {/* <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+                        Full-stack web developer with two years&apos; experience
+                        who is innovative and ambitious. Excellent analytical
+                        and creative abilities, with a heavy emphasis on writing
+                        well-documented code.
+                    </p> */}
+          <div className="flex flex-col mt-10 items-center justify-center sm:flex-row sm:space-x-4 gap-3">
+            <AnimatedText delay={0.6}>
+              <Link
+                href={"https://github.com/namanbarkiya"}
+                target="_blank"
+                className={cn(buttonVariants({ size: "lg" }))}
+              >
+                <Icons.gitHub className="w-4 h-4 mr-2" /> GitHub
+              </Link>
+            </AnimatedText>
+            <AnimatedText delay={0.8}>
+              <Link
+                href={"/contact"}
+                rel="noreferrer"
+                className={cn(
+                  buttonVariants({
+                    variant: "outline",
+                    size: "lg",
+                  })
+                )}
+              >
+                <Icons.contact className="w-4 h-4 mr-2" /> Contact
+              </Link>
+            </AnimatedText>
+          </div>
+          <AnimatedText delay={1.2}>
+            <Icons.chevronDown className="h-6 w-6 mt-10" />
+          </AnimatedText>
+        </div>
+      </section>
+      <AnimatedSection
+        className="container space-y-6 bg-muted py-10"
+        id="skills"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <AnimatedText
+            as="h2"
+            className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl"
+          >
+            {pagesConfig.skills.title}
+          </AnimatedText>
+          <AnimatedText
+            as="p"
+            delay={0.2}
+            className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7"
+          >
+            {pagesConfig.skills.description}
+          </AnimatedText>
+        </div>
+        <SkillsCard skills={featuredSkills} />
+        <AnimatedText delay={0.4} className="flex justify-center">
+          <Link href="/skills">
+            <Button variant={"outline"} className="rounded-xl">
+              <Icons.chevronDown className="mr-2 h-4 w-4" /> View All
+            </Button>
+          </Link>
+        </AnimatedText>
+        {/* <div className="mx-auto text-center md:max-w-[58rem]">
+                    <p className="leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+                        See all the relevant skills.
+                    </p>
+                </div> */}
+      </AnimatedSection>
+      <AnimatedSection
+        direction="right"
+        className="container space-y-6 py-10 my-14"
+        id="experience"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <AnimatedText
+            as="h2"
+            className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl"
+          >
+            {pagesConfig.experience.title}
+          </AnimatedText>
+          <AnimatedText
+            as="p"
+            delay={0.2}
+            className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7"
+          >
+            {pagesConfig.experience.description}
+          </AnimatedText>
+        </div>
+        <div className="mx-auto grid justify-center gap-4 md:w-full lg:grid-cols-3">
+          {featuredExperiences.map((exp, index) => (
+            <AnimatedSection
+              key={exp.id}
+              delay={0.1 * (index + 1)}
+              direction="up"
+            >
+              <ProjectCard project={exp} />
+            </AnimatedSection>
+          ))}
+        </div>
+        <AnimatedText delay={0.4} className="flex justify-center">
+          <Link href="/experience">
+            <Button variant={"outline"} className="rounded-xl">
+              <Icons.chevronDown className="mr-2 h-4 w-4" /> View All
+            </Button>
+          </Link>
+        </AnimatedText>
+        {/* <div className="mx-auto text-center md:max-w-[58rem]">
+                    <p className="leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+                        See all the relevant experiences.
+                    </p>
+                </div> */}
+      </AnimatedSection>
+      <AnimatedSection
+        direction="left"
+        className="container space-y-6 bg-muted py-10 my-14"
+        id="contributions"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <AnimatedText
+            as="h2"
+            className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl"
+          >
+            {pagesConfig.contributions.title}
+          </AnimatedText>
+          <AnimatedText
+            as="p"
+            delay={0.2}
+            className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7"
+          >
+            {pagesConfig.contributions.description}
+          </AnimatedText>
+        </div>
+        <div className="mx-auto justify-center gap-4 md:w-full lg:grid-cols-3">
+          <ContributionCard contributions={featuredContributions} />
+        </div>
+        <AnimatedText delay={0.4} className="flex justify-center">
+          <Link href="/contributions">
+            <Button variant={"outline"} className="rounded-xl">
+              <Icons.chevronDown className="mr-2 h-4 w-4" /> View All
+            </Button>
+          </Link>
+        </AnimatedText>
+        {/* <div className="mx-auto text-center md:max-w-[58rem]">
+                    <p className="leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+                        See all the relevant skills.
+                    </p>
+                </div> */}
+      </AnimatedSection>
+    </ClientPageWrapper>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,36 @@
+export const siteConfig = {
+  name: "Naman Barkiya Portfolio",
+  authorName: "Naman Barkiya",
+  username: "namanbarkiya",
+  description:
+    "Naman Barkiya is a Full Stack Developer, who loves to build web applications and is passionate about learning new technologies.",
+  url: "https://nbarkiya.xyz",
+  links: {
+    twitter: "https://x.com/namanbarkiya",
+    github: "https://github.com/namanbarkiya",
+  },
+  ogImage: "https://nbarkiya.xyz/naman-img.jpg",
+  iconIco:
+    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357384/assets/naman-favicon.ico",
+  logoIcon:
+    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357294/assets/naman-logo.png",
+  keywords: [
+    "Naman Barkiya",
+    "Naman Barkiya Portfolio",
+    "Naman Barkiya Blog",
+    "Naman Barkiya Projects",
+    "Naman Barkiya Experience",
+    "Naman Barkiya Resume",
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+    "Shadcn",
+    "Next SEO",
+    "Next Server Components",
+    "Next.js Portfolio",
+    "Next.js Blog",
+    "Next.js Starter",
+  ],
+};
